e40bc32bc83071b19503ef6807f24a7da7e6722c
Adam Scoble <adam.scoble@gmail.com>
2018-03-30 18:35:36 +1100

    Option to disable default commands

--- HUDConsole/Assets/Console/Source/Console.cs
+++ HUDConsole/Assets/Console/Source/Console.cs
@@ -59,6 +59,9 @@ namespace HUDConsole {
 		[Header("History")]
 		[SerializeField] private ConsoleHistory m_consoleHistory;
 
+		[Header("Default Commands")]
+		[SerializeField] private bool enableDefaultCommands = true;
+
 		[Header("Unity Log Settings")]
 		[SerializeField] private bool logUnityErrors = true;
 		[SerializeField] private bool logUnityAsserts = true;
@@ -85,12 +88,15 @@ namespace HUDConsole {
 			m_consoleView.transform.SetParent(transform, false);
 
 			// Add core commands.
+			AddCommand("Help", ConsoleCoreCommands.Help, "List of commands and their help text.");
+
+			if (!enableDefaultCommands) { return; }
+
 			AddCommand("Echo", ConsoleCoreCommands.Echo, "Display message to console.");
 			AddCommand("Console.Log", ConsoleCoreCommands.ConsoleLog, "Display message to console.");
 			AddCommand("Console.LogWarning", ConsoleCoreCommands.ConsoleLogWarning, "Display warning message to console.");
 			AddCommand("Console.LogError", ConsoleCoreCommands.ConsoleLogError, "Display error message to console.");
 			AddCommand("Console.Clear", ConsoleCoreCommands.ConsoleClear, "Clear console.");
-			AddCommand("Help", ConsoleCoreCommands.Help, "List of commands and their help text");
 
 			AddCommand("Debug.Log", ConsoleCoreCommands.DebugLog, "Logs message to the Unity Console.");
 			AddCommand("Debug.LogWarning", ConsoleCoreCommands.DebugLogWarning, "A variant of Debug.Log that logs a warning message to the console.");
